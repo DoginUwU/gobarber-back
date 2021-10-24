60e253f2d6ce39400267d561012be17f46333e43
Luiz Eduardo <doginuwu@gmail.com>
2021-10-24 11:11:41 -0300

    feat: add CreatUserService and make routes for it

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express';
+import CreateUserService from '../services/CreateUserService';
+
+const usersRouter = Router();
+
+usersRouter.post('/', async (request, response) => {
+    try {
+        const { name, email, password } = request.body;
+
+        const createUser = new CreateUserService();
+
+        const user = await createUser.execute({
+            name,
+            email,
+            password,
+        });
+
+        delete user.password;
+
+        return response.json(user);
+    } catch (error) {
+        if (error instanceof Error)
+            return response.status(400).json({ error: error.message });
+
+        return response.status(500);
+    }
+});
+
+export default usersRouter;
--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -4,12 +4,12 @@ import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 interface Request {
-    provider: string;
+    provider_id: string;
     date: Date;
 }
 
 class CreateAppointmentService {
-    public async execute({ provider, date }: Request): Promise<Appointment> {
+    public async execute({ provider_id, date }: Request): Promise<Appointment> {
         const appointmentsRepository = getCustomRepository(
             AppointmentsRepository,
         );
@@ -20,7 +20,7 @@ class CreateAppointmentService {
             throw Error('This appointment is already booked');
 
         const appointment = appointmentsRepository.create({
-            provider,
+            provider_id,
             date: appointmentDate,
         });
 
