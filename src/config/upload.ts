d2abea161cc52b629399683bde95b9809d9bde27
Luiz Eduardo <doginuwu@gmail.com>
2021-10-24 14:20:37 -0300

    feat: add update user avatar patch route

---
+++ src/config/upload.ts
@@ -0,0 +1,18 @@
+import path from 'path';
+import crypto from 'crypto';
+import multer from 'multer';
+
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
+
+export default {
+    directory: tmpFolder,
+    storage: multer.diskStorage({
+        destination: tmpFolder,
+        filename(request, file, fn) {
+            const fileHash = crypto.randomBytes(10).toString('hex');
+            const fileName = `${fileHash}-${file.originalname}`;
+
+            return fn(null, fileName);
+        },
+    }),
+};
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import CreateUserService from '../services/CreateUserService';
+import uploadConfig from '../config/upload';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
     try {
@@ -25,5 +30,32 @@ usersRouter.post('/', async (request, response) => {
         return response.status(500);
     }
 });
+
+usersRouter.patch(
+    '/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    async (request, response) => {
+        try {
+            const { id } = request.user;
+            const { file } = request;
+            const updateUserAvatar = new UpdateUserAvatarService();
+
+            const user = await updateUserAvatar.execute({
+                user_id: id,
+                avatarFilename: file?.filename,
+            });
+
+            delete user.password;
+
+            return response.json(user);
+        } catch (error) {
+            if (error instanceof Error)
+                return response.status(400).json({ error: error.message });
+
+            return response.status(500);
+        }
+    },
+);
 
 export default usersRouter;
