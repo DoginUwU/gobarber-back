2a4b509fbf2a090e77c0d54f1155bca76562d178
Luiz Eduardo <doginuwu@gmail.com>
2021-10-24 00:27:28 -0300

    refactor: remove business rules from appointments routes

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,27 +1,37 @@
-import { Router } from "express";
-import { startOfHour, parseISO } from "date-fns";
-import AppointmentsRepository from "../repositories/AppointmentsRepository";
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
 
-appointmentsRouter.get("/", (_, response) => {
+appointmentsRouter.get('/', (_, response) => {
     return response.json(appointmentsRepository.all());
 });
 
-appointmentsRouter.post("/", (request, response) => {
-    const { provider, date } = request.body;
+appointmentsRouter.post('/', (request, response) => {
+    try {
+        const { provider, date } = request.body;
 
-    const parsedDate = startOfHour(parseISO(date));
+        const parsedDate = parseISO(date);
 
-    if (appointmentsRepository.findByDate(parsedDate))
-        return response.status(400).json({
-            message: "This appointment is already booked",
+        const createAppointment = new CreateAppointmentService(
+            appointmentsRepository,
+        );
+
+        const appointment = createAppointment.execute({
+            provider,
+            date: parsedDate,
         });
 
-    const appointment = appointmentsRepository.create(provider, parsedDate);
+        return response.json(appointment);
+    } catch (error) {
+        if (error instanceof Error)
+            return response.status(400).json({ error: error.message });
 
-    return response.json(appointment);
+        return response.status(500);
+    }
 });
 
 export default appointmentsRouter;
