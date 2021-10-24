ecba9327f2f0c403d30472d05aabce1623461356
Luiz Eduardo <doginuwu@gmail.com>
2021-10-23 21:22:30 -0300

    feat: add express and initial appointments route

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import { uuid } from "uuidv4";
+import { startOfHour, parseISO } from "date-fns";
+
+const appointmentsRouter = Router();
+
+const appointments = [];
+
+appointmentsRouter.post("/", (request, response) => {
+    const { provider, date } = request.body;
+
+    const parsedDate = startOfHour(parseISO(date));
+
+    const appointment = {
+        id: uuid(),
+        provider,
+        date: parsedDate,
+    };
+
+    appointments.push(appointment);
+
+    return response.json(appointment);
+});
+
+export default appointmentsRouter;
