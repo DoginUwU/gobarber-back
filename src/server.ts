fe6228c4fba91c88a107fdda87fa2ee1e974ca43
Luiz Eduardo <doginuwu@gmail.com>
2021-10-24 14:25:26 -0300

    feat: add static files on express

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express from 'express';
 import dotenv from 'dotenv';
+import uploadConfig from './config/upload';
 import routes from './routes';
 
 import './database';
@@ -10,6 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
